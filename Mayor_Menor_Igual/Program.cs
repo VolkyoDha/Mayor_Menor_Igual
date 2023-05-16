5b767ae311b745c5e3de3dc96bd2f2ce337cefe1
The Wolf <clobodiaz@outlook.com>
2023-05-15 21:43:34 -0500

    Mayor_Menor_igual

---
+++ Mayor_Menor_Igual/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.WriteLine("Ingrese un número: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine(n1 + " es mayor a " + n2);
+            }
+            else if (n2 > n1)
+            {
+                Console.WriteLine(n2 + " es mayor a " + n1);
+            }
+            else
+            {
+                Console.WriteLine("Ambos números son iguales");
+            }
+            Console.ReadLine();
+        }
+    }
+}
